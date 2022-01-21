b7d2e208729d92633b5319b4ef963fde2d436739
Brandon Goberman <brndongoberman01@gmail.com>
2022-01-20 21:07:56 -0600

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+
+})
+ScrollReveal().reveal('.prin-new');
+ScrollReveal().reveal('.cards', { delay: 500 });
+ScrollReveal().reveal('.banner-cards', { delay: 500 });
